de1b9bee52acf19f69d6455e8a1c5c094d963885
Akash porwal <62378476+akash7883@users.noreply.github.com>
2025-04-24 19:33:06 +0000

    Datatype

---
+++ JS/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // this will treat all JS code as newer version
+
+// alert("Hello")  we are using nodejs not browser so there is a different way
+
+// Different Datatypes present in JS 
+/* 
+Primitive
+
+1. number -> (2**53)
+2. string -> can be used in '' or ""
+3. boolean -> true or false
+4. bigint
+5. null -> Standalone value and is considered as an object
+6. undefined -> declare not assigned
+7. symbol -> to find uniqueness in componenets
+
+8. objects
+
+*/
+
+
+let name1 = "akash" 
+let age = 13
+let isLoggedIn = true
+
+console.table([name1,typeof(name1),age,typeof(age),isLoggedIn,typeof(isLoggedIn),typeof(null),typeof(undefined)])
